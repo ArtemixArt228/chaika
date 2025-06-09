2321924c0fbeb8cb5ea6ede2fe00293d7758eb0c
ArtemixArt228 <artemix.portfolio@gmail.com>
2025-06-09 18:29:40 +0300

    Refactor HeroAdvantagesCards: simplify structure using a dynamic array and improve responsive design with CSS adjustments

--- src/app/(homepage)/hero-section/components/hero-advantages-cards.tsx
+++ src/app/(homepage)/hero-section/components/hero-advantages-cards.tsx
@@ -3,70 +3,61 @@ import Image from "next/image";
 
 import { Separator } from "@/components/ui/separator";
 
+const HERO_ADVANTAGES_CARDS = [
+  {
+    icon: "/homepage/hero-section/cards/return.svg",
+    alt: "Return icon",
+    title: "Натуральна сировина",
+    description: "Продукти, вирощені з любовʼю",
+  },
+  {
+    icon: "/homepage/hero-section/cards/tractor.svg",
+    alt: "Tractor icon",
+    title: "100% автономність",
+    description: "Закритий цикл виробництва",
+  },
+  {
+    icon: "/homepage/hero-section/cards/shop.svg",
+    alt: "Shop icon",
+    title: "Фірмові магазини",
+    description: "8 магазини з продукцією",
+  },
+];
+
 export const HeroAdvantagesCards = () => {
   return (
-    <div className="absolute z-10 -bottom-17 w-full">
-      <div className="container mx-auto">
-        <div className="flex h-36 px-12 py-10 bg-night-rider rounded-xl justify-between">
-          <div className="flex items-center gap-6">
-            <div className="h-16 w-16 relative shrink-0 overflow-hidden flex items-center justify-center">
-              <Image
-                className="h-full w-full overflow-hidden shrink-0 object-cover"
-                loading="lazy"
-                fill
-                alt="Return icon"
-                src="/homepage/hero-section/cards/return.svg"
-              />
-            </div>
-            <div className="flex flex-col items-start justify-center">
-              <h3 className="text-2xl uppercase text-gray-100">
-                Натуральна сировина
-              </h3>
-              <div className="text-base text-stone-400">
-                Продукти, вирощені з любовʼю
+    <div className="lg:absolute z-10 lg:-bottom-17 w-full">
+      <div className="lg:container mx-auto">
+        <div className="flex flex-col gap-10 p-2 lg:flex-row lg:h-36 lg:px-12 lg:py-10 bg-night-rider lg:rounded-xl lg:justify-between">
+          {HERO_ADVANTAGES_CARDS.map((card, index) => (
+            <React.Fragment key={index}>
+              <div className="flex flex-col lg:flex-row items-center gap-6">
+                <div className="h-16 w-16 relative shrink-0 overflow-hidden flex items-center justify-center">
+                  <Image
+                    className="h-full w-full overflow-hidden shrink-0 object-cover"
+                    loading="lazy"
+                    fill
+                    alt={card.alt}
+                    src={card.icon}
+                  />
+                </div>
+                <div className="flex flex-col items-center lg:items-start justify-center">
+                  <h3 className="text-2xl uppercase text-gray-100 text-center lg:text-left">
+                    {card.title}
+                  </h3>
+                  <p className="text-base text-stone-400 text-center lg:text-left">
+                    {card.description}
+                  </p>
+                </div>
               </div>
-            </div>
-          </div>
-          <Separator orientation="vertical" className="bg-gray-100" />
-          <div className="flex items-center gap-6">
-            <div className="h-16 w-16 relative shrink-0 overflow-hidden flex items-center justify-center">
-              <Image
-                className="h-full w-full overflow-hidden shrink-0 object-cover"
-                loading="lazy"
-                fill
-                alt="Tractor icon"
-                src="/homepage/hero-section/cards/tractor.svg"
-              />
-            </div>
-            <div className="flex flex-col items-start justify-center">
-              <h3 className="text-2xl uppercase text-gray-100">
-                100% автономність
-              </h3>
-              <div className="text-base text-stone-400">
-                Закритий цикл виробництва
-              </div>
-            </div>
-          </div>
-          <Separator orientation="vertical" className="bg-gray-100" />
-          <div className="flex items-center gap-6">
-            <div className="h-16 w-16 relative shrink-0 overflow-hidden flex items-center justify-center">
-              <Image
-                className="h-full w-full overflow-hidden shrink-0 object-cover"
-                loading="lazy"
-                fill
-                alt="Shop icon"
-                src="/homepage/hero-section/cards/shop.svg"
-              />
-            </div>
-            <div className="flex flex-col items-start justify-center">
-              <h3 className="text-2xl uppercase text-gray-100">
-                Фірмові магазини
-              </h3>
-              <div className="text-base text-stone-400">
-                8 магазини з продукцією
-              </div>
-            </div>
-          </div>
+              {index < HERO_ADVANTAGES_CARDS.length - 1 && (
+                <Separator
+                  orientation="vertical"
+                  className="bg-gray-100 hidden lg:block"
+                />
+              )}
+            </React.Fragment>
+          ))}
         </div>
       </div>
     </div>
