f071f7d0533ab367e3c7ae561c2f050873a0728a
ArtemixArt228 <artemix.portfolio@gmail.com>
2025-06-11 10:03:27 +0300

    Refactor HeroSection: improve responsiveness with updated breakpoints, adjust layout for better alignment, and refine text and image styles

--- src/app/about/hero-section/hero-section.tsx
+++ src/app/about/hero-section/hero-section.tsx
@@ -5,32 +5,31 @@ import { CounterCards } from "@/app/about/hero-section/components/counter-cards"
 
 export const HeroSection = () => {
   return (
-    <section className="min-h-dvh mt-32 mb-32">
-      <div className="py-8">
-        <div className="relative rounded-r-xl h-110 w-3xl">
-          <Image
-            src="/about/hero-section/field.png"
-            alt="Field"
-            fill
-            className="object-cover rounded-r-xl"
-          />
+    <section className="min-h-dvh mt-0 lg:mt-24 mb-32">
+      <div className="relative w-full h-screen lg:h-110 lg:w-3xl rounded-none lg:rounded-r-xl">
+        <Image
+          src="/about/hero-section/field.png"
+          alt="Field"
+          fill
+          className="object-cover rounded-none lg:rounded-r-xl"
+        />
 
-          <div className="absolute -right-full top-1/2 -translate-1/2 w-xl">
-            <div className="bg-night-rider rounded-xl text-center px-6 py-4">
-              <h1 className="text-3xl uppercase text-white">
-                Чайка – смак, вирощений з турботою.
-              </h1>
-              <p className="text-stone-400 text-xl">
-                Ми працюємо в агробізнесі, тваринництві, садівництві та
-                м’ясопереробці, забезпечуючи повний цикл виробництва з турботою
-                про природу і людей
-              </p>
-            </div>
+        <div className="absolute right-0 lg:-right-1/3 xl:-right-2/3 top-1/2 -translate-y-1/2 max-w-92 md:max-w-xl">
+          <div className="bg-night-rider rounded-l-xl lg:rounded-xl px-6 py-4">
+            <h1 className="text-2xl md:text-4xl uppercase text-white mb-6">
+              Чайка – смак,
+              <br /> вирощений з турботою.
+            </h1>
+            <p className="text-stone-400 text-base md:text-xl">
+              Ми працюємо в агробізнесі, тваринництві, садівництві та
+              м’ясопереробці, забезпечуючи повний цикл виробництва з турботою
+              про природу і людей
+            </p>
           </div>
         </div>
-
-        <CounterCards />
       </div>
+
+      <CounterCards />
     </section>
   );
 };
