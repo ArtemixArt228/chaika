85b2df54800cf53736afa221ac8cc096397ccc6b
ArtemixArt228 <artemix.portfolio@gmail.com>
2025-06-12 00:20:18 +0300

    Refactor HistorySection: enhance layout responsiveness, update text and image styles, adjust spacing and alignment for better visual consistency

--- src/app/about/history-section/components/history-item.tsx
+++ src/app/about/history-section/components/history-item.tsx
@@ -12,11 +12,13 @@ export const HistoryItem = ({
 }: THistoryItemProps) => {
   return (
     <div className="flex items-center gap-6 bg-black">
-      <div className="rounded-full bg-night-rider flex justify-center items-center w-20 h-20">
+      <div className="rounded-full bg-night-rider flex shrink-0 justify-center items-center w-20 h-20">
         <Image width={32} height={32} src={itemImage} alt="history item" />
       </div>
 
-      <p className="text-stone-400">{itemDescription}</p>
+      <p className="text-stone-400 max-w-md text-base md:text-lg">
+        {itemDescription}
+      </p>
     </div>
   );
 };
--- src/app/about/history-section/history-section.tsx
+++ src/app/about/history-section/history-section.tsx
@@ -30,46 +30,46 @@ const HISTORY_ITEMS = [
 
 export const HistorySection = () => {
   return (
-    <section className="container mx-auto mb-20">
-      <h4 className="text-center text-white uppercase text-4xl mb-8">
+    <section className="container mx-auto mb-20 relative">
+      <h4 className="text-center text-white uppercase text-2xl md:text-4xl mb-16">
         Історія створення та розвиток компанії
       </h4>
 
-      <div className="flex justify-between items-center gap-4">
+      <div className="flex justify-between items-start gap-4">
         <div className="relative">
           <Separator
             orientation="vertical"
-            className="bg-gray-100 absolute left-10 z-1"
+            className="bg-gray-100 absolute left-13 md:left-10 z-1"
           />
-          <div className="flex flex-col gap-6 z-10 relative">
+          <div className="flex flex-col gap-80 lg:gap-18 z-10 relative px-3 md:p-0">
             {HISTORY_ITEMS.map((item, i) => (
               <HistoryItem key={i} {...item} />
             ))}
           </div>
         </div>
 
-        <div>
-          <div className="w-110 h-55 relative rounded-xl">
+        <div className="flex flex-col absolute top-75 gap-48  sm:gap-43  sm:top-65 right-0 lg:top-0 lg:relative lg:block lg:pr-20">
+          <div className="relative w-70 h-55 md:w-100 md:h-60 rounded-l-xl md:rounded-xl">
             <Image
               fill
               src="/about/history-section/images/image-1.png"
               alt="history image"
-              className="object-cover rounded-xl"
+              className="object-cover rounded-l-xl md:rounded-xl"
             />
           </div>
-          <div className="w-110 h-55 relative rounded-xl -top-10 left-20">
+          <div className="relative w-70 h-55 md:w-100 md:h-60 rounded-l-xl md:rounded-xl lg:-top-20 lg:left-20">
             <Image
               src="/about/history-section/images/image-2.png"
               alt="history image"
-              className="object-cover rounded-xl"
+              className="object-cover rounded-l-xl md:rounded-xl"
               fill
             />
           </div>
-          <div className="w-110 h-55 relative rounded-xl -top-20">
+          <div className="relative w-70 h-55 md:w-100 md:h-60 rounded-l-xl md:rounded-xl lg:-top-30">
             <Image
               src="/about/history-section/images/image-3.png"
               alt="history image"
-              className="object-cover rounded-xl"
+              className="object-cover rounded-l-xl md:rounded-xl"
               fill
             />
           </div>
