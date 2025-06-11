99395969b28b2278ec30aeee0d08ef8e5f06ae9f
ArtemixArt228 <artemix.portfolio@gmail.com>
2025-06-12 01:08:24 +0300

    Refactor OurTeamSection and related components: update layout and styles for improved responsiveness, replace Swiper navigation buttons with custom Chevron icons, update SVG usage for quotation marks, and refine text and spacing adjustments in About and History sections.

--- src/app/about/social-sphere-section/social-sphere-section.tsx
+++ src/app/about/social-sphere-section/social-sphere-section.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export const SocialSphereSection = () => {
   return (
-    <section className="container mx-auto mb-20">
-      <h4 className="text-center text-white uppercase text-4xl mb-8">
+    <section className="container mx-auto mb-20 p-3 md:p-0">
+      <h4 className="text-center text-white uppercase text-2xl md:text-4xl mb-16">
         Соціальна сфера
       </h4>
-      <p className="text-stone-400 text-xl text-center">
+      <p className="text-stone-400 text-base md:text-xl text-center mb-20">
         Ми будуємо краще завтра, підтримуючи місцеві громади, зберігаючи
         традиції та впроваджуючи інновації у кожній сфері нашої діяльності
       </p>
-      <div className="relative rounded-xl h-110">
+      <div className="relative rounded-xl aspect-video md:aspect-auto md:h-110 ">
         <Image
           src="/about/social-sphere-section/banner.png"
           alt="Banner"
