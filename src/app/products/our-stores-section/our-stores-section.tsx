e1b7417cadba16320dc7149f319cd1a00a111263
ArtemixArt228 <artemix.portfolio@gmail.com>
2025-06-17 00:03:12 +0300

    Refactor OurStoresSection: update layout and styles for enhanced responsiveness, add city, location, and schedule details, adjust SwiperSlide structure, and replace image asset with new path.

--- src/app/products/our-stores-section/our-stores-section.tsx
+++ src/app/products/our-stores-section/our-stores-section.tsx
@@ -8,10 +8,13 @@ import Image from "next/image";
 const slides = [
   {
     id: 1,
-    image: "/about/our-team-section/image-1.png",
+    image: "/products/our-stores-section/image-1.png",
     title: "Мʼясний магазин  ”Чайка”",
     description:
       "Наш магазин пропонує асортимент свіжого м‘яса та готової м’ясної продукції",
+    city: "м.Луцьк,",
+    location: "вул. Ковельська 15, прос-т Перемоги 11",
+    schedule: "Пн-Сб 8:30 - 19:00 Нд 8:30 - 17:00",
   },
 ];
 
@@ -32,23 +35,36 @@ export const OurStoresSection = () => {
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
-            <div className="flex flex-col md:flex-row items-center justify-center gap-6">
-              <div className="relative w-80 h-80">
+            <div className="flex flex-col md:flex-row items-center justify-center">
+              <div className="relative w-full aspect-square md:w-150 md:h-142 md:left-10">
                 <Image
                   src={slide.image}
                   alt="team member"
                   fill
-                  className="rounded-xl w-full md:w-1/2 max-w-md object-cover"
+                  className="md:rounded-xl object-cover"
                 />
               </div>
 
-              <div className="bg-night-rider text-white rounded-xl p-6 w-full md:w-1/2 relative">
-                <h4 className="text-center text-white uppercase text-4xl mb-8">
+              <div className="relative bg-night-rider text-white rounded-xl p-6 w-10/12 bottom-10 lg:bottom-0 md:w-full md:max-w-md md:right-10">
+                <h6 className="text-white uppercase text-2xl lg:text-3xl mb-3">
                   {slide.title}
-                </h4>
-                <p className="text-stone-400 text-xl text-center">
+                </h6>
+                <p className="text-base md:text-xl text-stone-400 mb-6">
                   {slide.description}
                 </p>
+                <div className="flex flex-row lg:flex-col gap-4">
+                  <p className="text-base md:text-xl text-white mb-6 max-w-56">
+                    {slide.city}
+                    <br />
+                    {slide.location}
+                  </p>
+
+                  <p className="text-base md:text-xl text-white mb-6 max-w-48">
+                    Графік роботи:
+                    <br />
+                    {slide.schedule}
+                  </p>
+                </div>
               </div>
             </div>
           </SwiperSlide>
