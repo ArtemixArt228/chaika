3284cb74c5ef041b9a3ca8f6e76be5419ee73f97
ArtemixArt228 <artemix.portfolio@gmail.com>
2025-06-17 14:09:10 +0300

    Refactor layout.tsx: replace Geist fonts with Roboto_Condensed, update body class usage, adjust font subsets, and remove unused favicon asset.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Roboto_Condensed } from "next/font/google";
 
 import { Header } from "@/components/shared/header";
 import { Footer } from "@/components/shared/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import "./globals.css";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["cyrillic"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -28,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} bg-black`}>
+      <body className={`${robotoCondensed.className} bg-black`}>
         <Header />
         {children}
         <Footer />
